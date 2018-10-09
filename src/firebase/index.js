69999d798f0911c7b38e81c84824fb9025a4165d
deborah <wolhandlerdeb@gmail.com>
2018-10-09 13:06:12 +0300

    uploading images

---
+++ src/firebase/index.js
@@ -0,0 +1,23 @@
+import firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/database'
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBxG-SVvWBmlLE1GsWRkU2_ib_tt3cLPEM",
+    authDomain: "zikaronhai.firebaseapp.com",
+    databaseURL: "https://zikaronhai.firebaseio.com",
+    projectId: "zikaronhai",
+    storageBucket: "zikaronhai.appspot.com",
+    messagingSenderId: "212977596137"
+};
+
+firebase.initializeApp(config);
+
+const storage = firebase.storage();
+var fbData = firebase.database();
+const dBRefImages = fbData.ref().child('images');
+
+export {
+    storage, dBRefImages, firebase 
+}
