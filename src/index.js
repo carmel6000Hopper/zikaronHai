3906d0de488171680bd62941347f723ab2a86713
shira <shirarab211@gmail.com>
2018-10-11 14:01:38 +0300

    organized code - deleted unneccesary files and connected between ImageUpload.jsx and Camera.js so they will show on the same page

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import ImageUpload from './components/ImageUpload';
+import {Camera} from './takePicture/Camera.js';
 
-ReactDOM.render(<ImageUpload />, document.getElementById('root'));
+ReactDOM.render([<ImageUpload/>, <Camera/>], document.getElementById('root'));
+
 registerServiceWorker();
