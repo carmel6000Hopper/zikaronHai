fa43466c8edd8265901b2ca7a33186d8b6c2f8b9
shirarab <broomonster@gmail.com>
2018-11-08 19:56:01 +0200

    changes navbar

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -27,16 +27,16 @@ export class Menu extends Component {
         <div ref={this.navSignedInRef} id="mySidebar" className="sidebar">
             {/* <a href="javascript:void(0)" className="closebtn" onClick={this.closeNav}>×</a> */}
             {/* <img src={x} href="javascript:void(0)" className="closebtn" onClick={this.closeNav} /> */}
-            <a href="javascript:void(0)" onClick={this.closeNav}>
+            <a href="javascript:void(0)" onClick={() => this.closeNav(this.navSignedInRef)}>
                 <img src={menuExitBtn} className="closebtn" alt="close-btn" />
             </a>
             <div className="a-container">
-                <Link to={'/camera'} onClick={this.closeNav}>הוספת שלט</Link>
-                <Link to={'/upload'} onClick={this.closeNav}>upload</Link>
+                <Link to={'/camera'} onClick={() => this.closeNav(this.navSignedInRef)}>הוספת שלט</Link>
+                <Link to={'/upload'} onClick={() => this.closeNav(this.navSignedInRef)}>upload</Link>
                 <a>אודות</a>
-                <Link to={'/gps'} onClick={this.closeNav}>מפה</Link>
-                <Link to={'/account'} onClick={this.closeNav}>החשבון שלי</Link>
-                <Link to={'/signout'} onClick={this.closeNav}>התנתק</Link>
+                <Link to={'/gps'} onClick={() => this.closeNav(this.navSignedInRef)}>מפה</Link>
+                <Link to={'/account'} onClick={() => this.closeNav(this.navSignedInRef)}>החשבון שלי</Link>
+                <Link to={'/signout'} onClick={() => this.closeNav(this.navSignedInRef)}>התנתק</Link>
             </div>
         </div>
 
@@ -44,13 +44,13 @@ export class Menu extends Component {
         <div ref={this.navVisitorRef} id="mySidebar" className="sidebar">
             {/* <a href="javascript:void(0)" className="closebtn" onClick={this.closeNav}>×</a> */}
             {/* <img src={x} href="javascript:void(0)" className="closebtn" onClick={this.closeNav} /> */}
-            <a href="javascript:void(0)" onClick={this.closeNav}>
+            <a href="javascript:void(0)" onClick={() => this.closeNav(this.navVisitorRef)}>
                 <img src={menuExitBtn} className="closebtn" alt="close-btn" />
             </a>
             <div className="a-container">
                 <a>אודות</a>
-                <Link to={'/signin'} onClick={this.closeNav}>כניסה כמשתמש</Link>
-                <Link to={'/gps'} onClick={this.closeNav}>מפה</Link>
+                <Link to={'/signin'} onClick={() => this.closeNav(this.navVisitorRef)}>כניסה כמשתמש</Link>
+                <Link to={'/gps'} onClick={() => this.closeNav(this.navVisitorRef)}>מפה</Link>
             </div>
         </div>
 
@@ -59,17 +59,14 @@ export class Menu extends Component {
         return (
             <div>
                 <AuthConsumer>
-                    {isAuth => isAuth
-                        ? <this.MenuSignedIn />
-                        : <this.MenuVisitor />
-                    }
-                    {isAuth => isAuth ?
-                        (<div id="main">
-                            <button className="openbtn" onClick={()=>this.openNav(this.navSignedInRef)}>☰ זכרון חי</button>
+                    {isAuth => isAuth === true
+                        ? (<div id="main"> <this.MenuSignedIn />
+                            <button className="openbtn" onClick={() => this.openNav(this.navSignedInRef)}>☰ זכרון חי</button>
                         </div>)
-                        : (<div id="main">
-                            <button className="openbtn" onClick={()=>this.openNav(this.navVisitorRef)}>☰ זכרון חי</button>
-                        </div>)}
+                        : (<div id="main"> <this.MenuVisitor />
+                            <button className="openbtn" onClick={() => this.openNav(this.navVisitorRef)}>☰ זכרון חי</button>
+                        </div>)
+                    }
                 </AuthConsumer>
 
 
