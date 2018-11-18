1530dc2f918c2f9260e5000a135acfb88408f06d
Noga Levy <nogalevy2@gmail.com>
2018-11-18 15:36:24 +0200

    add userId from firebase to onMarkerClick

--- src/components/LocationGPS.js
+++ src/components/LocationGPS.js
@@ -8,6 +8,7 @@ export class LocationGPS extends Component {
         super(props);
         this.state = {
             outputPlace: "",
+            userIdUpload: "",
             imagePlaceSrc: "",
             currPlaceName: "",
             markersPlace: [],
@@ -17,16 +18,6 @@ export class LocationGPS extends Component {
 
     componentDidMount() {
         this.getPlacesCoordinates();
-    }
-
-    getPlaceImage = (key) => {
-        const stor = firebase.storage();
-        const storRef = stor.ref('images/' + key);
-        storRef.getDownloadURL().then( (url) => {
-            this.setState({ imagePlaceSrc: url })
-          }).catch(function(error) {
-            console.log(error)
-          })
     }
 
     getPlacesCoordinates = () => {
@@ -39,7 +30,7 @@ export class LocationGPS extends Component {
             let keys = Object.keys(images)
             var i = 0
             imagesValues.map((image) =>
-                Places.push({ lng: image.gps_longitude, lat: image.gps_latitude, placeKey: keys[i], placeName: image.name },
+                Places.push({ lng: image.gps_longitude, lat: image.gps_latitude, placeKey: keys[i], placeName: image.name, userId: image.userId },
                     i = i + 1)
             )
 
@@ -48,33 +39,57 @@ export class LocationGPS extends Component {
         dBRefImages.off("value");
     }
 
-    onMarkerClicked = (placeKey, placeName) => {
+
+    // onMarkerClicked get the placeKey and the placeName
+    // change the state to the marker information (imagePlaceSrc and placeName) from the fire base
+    onMarkerClicked = (placeKey, placeName, userId) => {
         let outputPlace = this.state.outputPlace;
         let imagePlaceSrc = this.state.imagePlaceSrc;
         let currPlaceName = this.state.currPlaceName;
         let currPlaceKey = this.state.currPlaceKey;
+        let userIdUpload = this.state.userIdUpload;
         if (currPlaceKey !== placeKey) {
-            outputPlace = "Place name " + placeName + " that need to change ";
+            outputPlace = "Place name: " + placeName;
             this.getPlaceImage(placeKey);
             currPlaceKey = placeKey;
             currPlaceName = placeName;
+            userIdUpload = userId;
+            console.log("user name is: ", userId)
             console.log("cuurent place is: ", currPlaceName);
             this.setState({ outputPlace: outputPlace });
             this.setState({ currPlaceKey: currPlaceKey });
+            this.setState({userIdUpload: userIdUpload});
+            // this.setState({ currPlaceName: currPlaceName });
         }
         else {
             outputPlace = "";
             imagePlaceSrc = "";
             currPlaceName = "";
+            userIdUpload = "";
             console.log("cuurent place is: ", currPlaceName);
             this.setState({ outputPlace: outputPlace });
             this.setState({ imagePlaceSrc: imagePlaceSrc });
             this.setState({ currPlaceName: currPlaceName });
+            this.setState({userIdUpload: userIdUpload});
         }
 
         console.log("img src: ", this.state.imagePlaceSrc);
     }
 
+
+    getPlaceImage = (key) => {
+        const stor = firebase.storage();
+        const storRef = stor.ref('images/' + key);
+        storRef.getDownloadURL().then( (url) => {
+            this.setState({ imagePlaceSrc: url })
+          }).catch(function(error) {
+            console.log(error)
+          })
+    }
+    
+
+
+    
 
     render() {
         return (
@@ -90,9 +105,11 @@ export class LocationGPS extends Component {
                     :
                     <div>
                         <h3>{this.state.outputPlace}</h3>
+                        <h3>Upload by: {this.state.userIdUpload}</h3>
                         {/* <h3>{this.state.imagePlaceSrc}</h3> */}
                         <img src={this.state.imagePlaceSrc}></img>
                     </div>}
+        
             </div>
         );
     }
